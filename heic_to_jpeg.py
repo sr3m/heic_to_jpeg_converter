707a1f58160eb6aeb44a3b6640892ef4820f6559
sr3m <69061414+sr3m@users.noreply.github.com>
2025-06-01 13:40:03 -0400

    Add files via upload
    This a mass heic to jpeg converter. The *.py file shall be placed in the folder with the .heic pictures and execute the algorithm through the command prompt. The algorithm will create a new folder called "nueva_extension" and will include all the photos in *.jpeg format.

---
+++ heic_to_jpeg.py
@@ -0,0 +1,45 @@
+import os
+import pathlib
+import shutil
+from pillow_heif import register_heif_opener
+import time
+from PIL import Image
+
+register_heif_opener() #plugin for pillow to open heic
+
+
+dir_list = os.listdir(os.getcwd())   
+main_folders = list()
+floors_per_plants = dict()
+all_photos_names_list = list()
+new_extension = ".jpeg"
+concat_photos_path = "nueva_extension"
+
+# execute_question = input("What to execute the algorithm? (Y/n) ")
+
+def photo_scrapper():
+    start = time.time()
+    for i in dir_list:
+        filename,extension_name = os.path.splitext(i)
+        if extension_name == ".heic":
+            src_path = os.path.join(os.getcwd(),f"{i}")
+            photos_name = filename
+            dst_path = f".\\{concat_photos_path}\\{photos_name}"
+            with Image.open(src_path, mode="r") as im:
+                im.convert("RGB").save(dst_path + new_extension,"JPEG")
+    
+    finish = time.time()
+    return print(f"Completed in {round(finish - start,2)} seg")
+
+# if execute_question == 'Y' or execute_question == 'y':
+if os.path.exists(".\\" + concat_photos_path):
+    pass
+else:
+    os.makedirs(".\\" + concat_photos_path)
+
+photo_scrapper()
+
+# print(dir_list)
+
+print("Thanks for using sr3m's image extractor algorithm")
+    
